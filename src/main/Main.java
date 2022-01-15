5e678ff1ddc4ff7b389b886422caf7d1e0605bcf
Vedran Pehlivanovic <vedran.pehlivanovic@gmail.com>
2022-01-15 15:35:02 -0500

    moved test code into method

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,10 @@ import stack.Stack;
 public class Main {
 
 	public static void main(String[] args) {
+		testStack();
+	}
+	
+	public static void testStack() {
 		Stack stack = new Stack(5);
 		stack.push(10);
 		stack.push(20);
